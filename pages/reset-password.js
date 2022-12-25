13697e5d0c0a4f31f09ad35cea390b30f2609c4a
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-25 08:36:25 +0700

    add responsive reset-password page

--- pages/reset-password.js
+++ pages/reset-password.js
@@ -16,7 +16,7 @@ const ForgotPassword = () => {
     <>
       <div className="flex m-0 h-screen">
         {/* container left */}
-        <div className="w-1/2 bg-[#B1B2FF] px-12 py-5 h-screen text-white relative h-screen overflow-y-scroll">
+        <div className="w-1/2 bg-[#B1B2FF] px-12 py-5 h-screen text-white relative h-screen overflow-y-scroll hidden md:block">
           <div className="font-bold	text-3xl">YukPay</div>
           <Image src={line} alt="line" className="absolute top-[0] left-[0]" />
           <Image src={phone} alt="Image"></Image>
@@ -31,70 +31,81 @@ const ForgotPassword = () => {
           </div>
         </div>
         {/* container right */}
-        <div className="w-1/2 px-12 py-20 bg-[#FAFCFF] h-screen overflow-y-scroll flex flex-col">
-          <div className="font-bold	text-xl mb-10">
-            Did You Forgot Your Password? Don’t Worry, You Can Reset Your
-            Password In a Minutes.
+        <div className="w-full md:w-1/2 md:px-12 px-0 md:py-20 pb-0 pt-20 bg-[#FAFCFF] h-screen overflow-y-scroll">
+          <div className="block md:hidden text-xl font-bold text-center text-[#B1B2FF] mb-10">
+            YukPay
           </div>
-          <div className="text-base text-[#3A3D4299] mb-5">
-            Now you can create a new password for your YukPay account. Type
-            your password twice so we can confirm your new passsword.
+          <div className="bg-white rounded-t-3xl md:bg-[#FAFCFF] md:text-left text-center md:px-0 px-2 md:py-0 py-5">
+            <div className="font-bold	text-xl mb-10 hidden md:block">
+              Did You Forgot Your Password? Don’t Worry, You Can Reset Your
+              Password In a Minutes.
+            </div>
+            <div className="font-bold	text-xl mb-10 block md:hidden">
+              Reset Password
+            </div>
+            <div className="text-base text-[#3A3D4299] mb-5 hidden md:block">
+              Now you can create a new password for your YukPay account. Type
+              your password twice so we can confirm your new passsword.
+            </div>
+            <div className="text-base text-[#3A3D4299] mb-5 block md:hidden">
+              Create and confirm your new password so you can login to YukPay.
+            </div>
+            <form>
+              <div className="relative mb-5">
+                <input
+                  name="password"
+                  type={type}
+                  className="py-3 px-12 w-full bg-white md:bg-[#FAFCFF] w-full border-b-2 focus:outline-none"
+                  placeholder="Enter your password"
+                ></input>
+                <Lock
+                  style={{ color: "#A9A9A999" }}
+                  className="absolute top-[23%]"
+                />
+                {type === "password" ? (
+                  <EyeOff
+                    className="absolute right-0 top-[23%] r cursor-pointer"
+                    style={{ color: "#A9A9A999" }}
+                    onClick={showPassword}
+                  />
+                ) : (
+                  <Eye
+                    className="absolute right-0 top-[23%] r cursor-pointer"
+                    style={{ color: "#A9A9A999" }}
+                    onClick={showPassword}
+                  />
+                )}
+              </div>
+              <div className="relative mb-5">
+                <input
+                  name="confirmPassword"
+                  type={type}
+                  className="py-3 px-12 w-full bg-white md:bg-[#FAFCFF] w-full border-b-2 focus:outline-none"
+                  placeholder="Enter your confirm password"
+                ></input>
+                <Lock
+                  style={{ color: "#A9A9A999" }}
+                  className="absolute top-[23%]"
+                />
+                {type === "password" ? (
+                  <EyeOff
+                    className="absolute right-0 top-[23%] r cursor-pointer"
+                    style={{ color: "#A9A9A999" }}
+                    onClick={showPassword}
+                  />
+                ) : (
+                  <Eye
+                    className="absolute right-0 top-[23%] r cursor-pointer"
+                    style={{ color: "#A9A9A999" }}
+                    onClick={showPassword}
+                  />
+                )}
+              </div>
+            </form>
+            <button className="w-full bg-[#B1B2FF] rounded-md py-3 text-lg font-bold mt-10 text-white">
+              Reset Password
+            </button>
           </div>
-          <form>
-            <div className="relative mb-5">
-              <input
-                name="password"
-                type={type}
-                className="py-3 px-12 w-full bg-[#FAFCFF] w-full border-b-2 focus:outline-none"
-                placeholder="Enter your password"
-              ></input>
-              <Lock
-                style={{ color: "#A9A9A999" }}
-                className="absolute top-[23%]"
-              />
-              {type === "password" ? (
-                <EyeOff
-                  className="absolute right-0 top-[23%] r cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              ) : (
-                <Eye
-                  className="absolute right-0 top-[23%] r cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              )}
-            </div>
-            <div className="relative mb-5">
-              <input
-                name="confirmPassword"
-                type={type}
-                className="py-3 px-12 w-full bg-[#FAFCFF] w-full border-b-2 focus:outline-none"
-                placeholder="Enter your confirm password"
-              ></input>
-              <Lock
-                style={{ color: "#A9A9A999" }}
-                className="absolute top-[23%]"
-              />
-              {type === "password" ? (
-                <EyeOff
-                  className="absolute right-0 top-[23%] r cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              ) : (
-                <Eye
-                  className="absolute right-0 top-[23%] r cursor-pointer"
-                  style={{ color: "#A9A9A999" }}
-                  onClick={showPassword}
-                />
-              )}
-            </div>
-          </form>
-          <button className="w-full bg-[#B1B2FF] rounded-md py-3 text-lg font-bold mt-10 text-white">
-            Reset Password
-          </button>
         </div>
       </div>
     </>
