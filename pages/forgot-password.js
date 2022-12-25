23633983616e8acccd4c3bd8f841e446c187bb90
Putut Nardianto L <pututnardiantol@gmail.com>
2022-12-25 08:43:16 +0700

    add responsive forgot-password page

--- pages/forgot-password.js
+++ pages/forgot-password.js
@@ -10,7 +10,7 @@ const ForgotPassword = () => {
     <>
       <div className="flex m-0 h-screen">
         {/* container left */}
-        <div className="w-1/2 bg-[#B1B2FF] px-12 py-5 h-screen text-white relative h-screen overflow-y-scroll">
+        <div className="w-1/2 bg-[#B1B2FF] px-12 py-5 h-screen text-white relative h-screen overflow-y-scroll hidden md:block">
           <div className="font-bold	text-3xl">YukPay</div>
           <Image src={line} alt="line" className="absolute top-[0] left-[0]" />
           <Image src={phone} alt="Image"></Image>
@@ -25,34 +25,44 @@ const ForgotPassword = () => {
           </div>
         </div>
         {/* container right */}
-        <div className="w-1/2 px-12 py-20 bg-[#FAFCFF] h-screen overflow-y-scroll flex flex-col">
-          <div className="font-bold	text-xl mb-10">
-            Did You Forgot Your Password? Don’t Worry, You Can Reset Your
-            Password In a Minutes.
+        <div className="w-full md:w-1/2 md:px-12 px-0 md:py-20 pb-0 pt-20 bg-[#FAFCFF] h-screen overflow-y-scroll ">
+          <div className="block md:hidden text-xl font-bold text-center text-[#B1B2FF] mb-10">
+            YukPay
           </div>
-          <div className="text-base text-[#3A3D4299] mb-5">
-            To reset your password, you must type your e-mail and we will send a
-            link to your email and you will be directed to the reset password
-            screens.
-          </div>
-          <form>
-            <div className="relative mb-5">
-              <input
-                name="email"
-                type="email"
-                className="w-full border-b-2 focus:outline-none px-12 py-3 bg-[#FAFCFF]"
-                placeholder="Enter your e-mail"
-              ></input>
-              <Mail
-                style={{ color: "#A9A9A999" }}
-                className="absolute top-[23%]"
-              />
+          <div className="bg-white rounded-t-3xl md:bg-[#FAFCFF] md:text-left text-center md:px-0 px-2 md:py-0 py-5">
+            <div className="font-bold	text-xl mb-10 hidden md:block">
+              Did You Forgot Your Password? Don’t Worry, You Can Reset Your
+              Password In a Minutes.
             </div>
-          </form>
-
-          <button className="w-full bg-[#B1B2FF] rounded-md py-3 text-lg font-bold mt-10 text-white">
-            Confirm
-          </button>
+            <div className="font-bold	text-xl mb-10 block md:hidden">
+              Reset Password
+            </div>
+            <div className="text-base text-[#3A3D4299] mb-5 hidden md:block">
+              To reset your password, you must type your e-mail and we will send
+              a link to your email and you will be directed to the reset
+              password screens.
+            </div>
+            <div className="text-base text-[#3A3D4299] mb-5 block md:hidden">
+              Enter your YukPay e-mail so we can send you a password reset link.
+            </div>
+            <form>
+              <div className="relative mb-5">
+                <input
+                  name="email"
+                  type="email"
+                  className="w-full border-b-2 focus:outline-none px-12 py-3 bg-white md:bg-[#FAFCFF]"
+                  placeholder="Enter your e-mail"
+                ></input>
+                <Mail
+                  style={{ color: "#A9A9A999" }}
+                  className="absolute top-[23%]"
+                />
+              </div>
+            </form>
+            <button className="w-full bg-[#B1B2FF] rounded-md py-3 text-lg font-bold mt-10 text-white">
+              Confirm
+            </button>
+          </div>
         </div>
       </div>
     </>
